ba5c8ed418824c534aad802ed15bf9d4ed83d43d
Pavlin Minkov <72264750+pavlinminkov@users.noreply.github.com>
2023-02-03 20:39:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+async function fetchImages() {
+  const response = await fetch("https://api.github.com/repos/pavlinminkov/images/contents/images");
+  const data = await response.json();
+  const images = data.filter(file => file.name.endsWith(".jpg") || file.name.endsWith(".png"));
+  const imageContainer = document.getElementById("gallery");
+  for (const image of images) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    const img = document.createElement("img");
+    img.src = image.download_url;
+    img.classList.add("img-fluid", "h-100");
+    row.appendChild(img);
+    imageContainer.appendChild(row);
+  }
+}
